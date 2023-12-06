a814b8480e1e5f326c74ac0057e4c5bbb61731cf
De-sam <samsoncoded@gmail.com>
2023-12-06 22:53:07 +0100

    UPDATEED: Basemodel and test base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,23 +1,37 @@
 #!/usr/bin/python3
 """
-This is the base class model to be inherited by other classes
+Module containing the BaseModel class.
 """
 import uuid
 from datetime import datetime
 
 class BaseModel:
+    """
+    BaseModel class defines all common attributes/methods for other classes.
+    """
     def __init__(self):
+        """
+        Initializes a new instance of the BaseModel class.
+        """
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
+        """
+        Returns a string representation of the BaseModel instance.
+        """
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
+        """
+        Updates the public instance attribute updated_at with the current datetime.
+        """
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """
+        Returns a dictionary containing all keys/values of __dict__ of the instance.
+        """
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,19 +1,69 @@
 #!/usr/bin/python3
+"""
+Module containing tests for the BaseModel class.
+"""
+import unittest
+from datetime import datetime
 
 from models.base_model import BaseModel
 
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
 
-print(my_model)
+class TestBaseModel(unittest.TestCase):
+    """
+    Test cases for the BaseModel class.
+    """
+    def test_base_model_instance(self):
+        """
+        Test creation of a BaseModel instance.
+        """
+        my_model = BaseModel()
+        my_model.name = "My First Model"
+        my_model.my_number = 89
 
-my_model.save()
-print(my_model)
+        self.assertEqual(type(my_model), BaseModel)
+        self.assertEqual(type(my_model.id), str)
+        self.assertEqual(type(my_model.created_at), datetime)
+        self.assertEqual(type(my_model.updated_at), datetime)
+        self.assertEqual(type(my_model.name), str)
+        self.assertEqual(type(my_model.my_number), int)
 
-my_model_json = my_model.to_dict()
-print(my_model_json)
+    def test_base_model_str(self):
+        """
+        Test the __str__ method of BaseModel.
+        """
+        my_model = BaseModel()
+        my_model.name = "My First Model"
+        my_model.my_number = 89
 
-print("JSON of my_model:")
-for key, value in my_model_json.items():
-        print("\t{}: ({}) - {}".format(key, type(value), value))
+        expected_str = "[BaseModel] ({}) {}".format(my_model.id, my_model.__dict__)
+        self.assertEqual(str(my_model), expected_str)
+
+    def test_base_model_save(self):
+        """
+        Test the save method of BaseModel.
+        """
+        my_model = BaseModel()
+        updated_at_before = my_model.updated_at
+        my_model.save()
+        updated_at_after = my_model.updated_at
+
+        self.assertNotEqual(updated_at_before, updated_at_after)
+
+    def test_base_model_to_dict(self):
+        """
+        Test the to_dict method of BaseModel.
+        """
+        my_model = BaseModel()
+        my_model.name = "My First Model"
+        my_model.my_number = 89
+
+        obj_dict = my_model.to_dict()
+
+        self.assertEqual(obj_dict['__class__'], 'BaseModel')
+        self.assertEqual(obj_dict['created_at'], my_model.created_at.isoformat())
+        self.assertEqual(obj_dict['updated_at'], my_model.updated_at.isoformat())
+        self.assertEqual(obj_dict['name'], 'My First Model')
+        self.assertEqual(obj_dict['my_number'], 89)
+
+if __name__ == '__main__':
+    unittest.main()
