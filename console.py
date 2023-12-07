9e88d0ac94519f461127eac70b9fe4d3e2d1ca5b
De-sam <samsoncoded@gmail.com>
2023-12-07 09:44:50 +0100

    UPDATEED: Basemodel based on pepe8 style

--- console.py
+++ console.py
@@ -2,37 +2,39 @@
 """Define a cmd class"""
 import cmd
 
+
 class Cmd(cmd.Cmd):
-	""" Console for the hbnb app"""
-	prompt = "(hbnb)"
+    """ Console for the hbnb app"""
+    prompt = "(hbnb)"
 
-	def do_quit(self, line):
-		"""
-		Quits or exits the console in interative mode
-		
-		PARAMETERS:
-		--self: An instance of the console processor
-		--line: The 'quit' command input from the user
+    def do_quit(self, line):
+        """
+        Quits or exits the console in interative mode
 
-		RETURNS:
-		--True: Exits for the cmdloop
-		"""
-		return True
-	"""alias for the qyit command"""
-	do_exit = do_quit
-	
-	def do_EOF(self, line):
-		"""
-		Handles the 'EOD' command to exit the console
+        PARAMETERS:
+        --self: An instance of the console processor
+        --line: The 'quit' command input from the user
 
-		PARAMETERS:
-		--self: An instance of the console processor
-		--line: The input from the user containing the 'EOF'
+        RETURNS:
+        --True: Exits for the cmdloop
+        """
+        return True
+    """alias for the qyit command"""
+    do_exit = do_quit
 
-		RETURNS:
-		--True: Signals the console to quit
-		"""
-		return True
+    def do_EOF(self, line):
+        """
+        Handles the 'EOD' command to exit the console
+
+        PARAMETERS:
+        --self: An instance of the console processor
+        --line: The input from the user containing the 'EOF'
+
+        RETURNS:
+        --True: Signals the console to quit
+        """
+        return True
+
 
 if __name__ == '__main__':
-	Cmd().cmdloop()
+    Cmd().cmdloop()
--- models/base_model.py
+++ models/base_model.py
@@ -5,10 +5,12 @@ Module containing the BaseModel class.
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     """
     BaseModel class defines all common attributes/methods for other classes.
     """
+
     def __init__(self):
         """
         Initializes a new instance of the BaseModel class.
@@ -20,18 +22,20 @@ class BaseModel:
         """
         Returns a string representation of the BaseModel instance.
         """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(
+                self.__class__.__name__, self.id, self.__dict__
+        )
 
     def save(self):
-        """
-        Updates the public instance attribute updated_at with the current datetime.
-        """
+        """updates the public instance
+updated_at with current time and saves it to database."""
         self.updated_at = datetime.now()
 
     def to_dict(self):
         """
-        Returns a dictionary containing all keys/values of __dict__ of the instance.
-        """
+        Returns a dictionary containing
+all keys/values of __dict__ of the instance.
+"""
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
