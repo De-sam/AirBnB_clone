6fd50df5e8671a571e0b66470b5c9a516ed00362
De-sam <samsoncoded@gmail.com>
2023-12-10 23:55:03 +0100

    updated readme and filestorage.py

--- README.md
+++ README.md
@@ -1,5 +1,53 @@
-description of the project
-description of the command interpreter:
-how to start it
-how to use it
-examples
+
+# AirBnB Clone Command-Line Interpreter
+
+## Project Description
+
+Welcome to the AirBnB clone project! This initial step involves creating a command-line interpreter. The primary goal is to manage AirBnB objects through commands, enabling users to perform actions such as creating new objects, retrieving objects, and updating attributes.
+
+## Command Interpreter
+
+The command interpreter allows users to interact with AirBnB objects using a set of commands.
+
+### How to Start
+
+To launch the command-line interpreter, run the `console.py` script:
+
+```bash
+$ ./console.py
+```
+
+### How to Use
+
+Once the interpreter is running, you can use the following commands:
+
+- `help`: Display a list of documented commands.
+- `quit`: Exit the command-line interpreter.
+
+### Examples
+
+You can interact with the interpreter both interactively and in non-interactive mode:
+
+Interactive mode:
+
+```bash
+$ ./console.py
+(hbnb) help
+# ... (commands output)
+(hbnb) quit
+$
+```
+
+Non-interactive mode:
+
+```bash
+$ echo "help" | ./console.py
+(hbnb)
+# ... (commands output)
+$
+```
+
+
+
+
+
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,35 +1,57 @@
+#!/usr/bin/python3
+"""
+Module that defines the FileStorage class responsible for serialization
+and deserialization of instances to and from a JSON file.
+"""
+
 import json
-from os import path
 from models.base_model import BaseModel
 
 class FileStorage:
+    """
+    The FileStorage class manages the serialization and deserialization
+    of instances to and from a JSON file.
+    """
+
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        """
+        Returns the dictionary containing all serialized objects.
+        """
         return self.__objects
 
     def new(self, obj):
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        """
+        Adds a new object to the dictionary of serialized objects.
+
+        Parameters:
+        obj (BaseModel): The BaseModel object to be serialized and added.
+        """
+        key = f"{obj.__class__.__name__}.{obj.id}"
         self.__objects[key] = obj
 
     def save(self):
-        obj_dict = {}
-        for key, obj in self.__objects.items():
-            obj_dict[key] = obj.to_dict()
-
-        with open(self.__file_path, 'w') as file:
-            json.dump(obj_dict, file)
+        """
+        Serializes the objects dictionary and writes it to the JSON file.
+        """
+        obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
+        with open(self.__file_path, mode='w') as json_file:
+            json.dump(obj_dict, json_file)
 
     def reload(self):
-        if path.exists(self.__file_path):
-            with open(self.__file_path, 'r') as file:
-                obj_dict = json.load(file)
+        """
+        Deserializes the objects from the JSON file and populates the objects dictionary.
+        If the file doesn't exist, no exception is raised.
+        """
+        try:
+            with open(self.__file_path, encoding="utf-8") as json_file:
+                obj_dict = json.load(json_file)
 
             for key, obj_data in obj_dict.items():
                 class_name, obj_id = key.split('.')
                 obj = BaseModel(**obj_data)
                 self.__objects[key] = obj
-
-storage = FileStorage()
-storage.reload()
+        except FileNotFoundError:
+            return
